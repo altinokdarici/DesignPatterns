0efd571a6f2d16aa28e15d796f52be7fa6282566
Altinok Darici <altinok.darici@outlook.com>
2015-09-16 15:46:49 +0300

    +Singleton

---
+++ CreationalPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+
+    class SocketManager
+    {
+        public DateTime CreatedAt { get; private set; }
+        public SocketManager()
+        {
+            CreatedAt = DateTime.Now;
+        }
+        
+        /// <summary>
+        /// Bu degisken sayesinde uygulama acik oldugu surece SocketManager hayatta kalacak ve bir daha (hata yapılmadığı sürece) yeni bir ornegi yaratılmayacak.
+        /// </summary>
+        private static SocketManager activeConnection;
+        public static SocketManager GetSocketManager()
+        {
+            //eger daha once connection yaratilmamis ise yeni bir connection yaratiyoruz.
+            if (activeConnection == null)
+                activeConnection = new SocketManager();
+
+            //yeni yaratilan veya zaten var olan, sonuc olarak tek bir tane olan active connection'i return ediyoruz.
+            return activeConnection;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                // dongu icerisinde surekli olarak getsocketmanager kullanıyorum, geri donen socket manager ilk defasında yaaratılan oluyor. 
+                //Bu gibi durumlarda Singleton kullanmak ileride developerlarin hata yapmasına engel oluyor.
+                SocketManager sm = SocketManager.GetSocketManager();
+
+                Console.WriteLine("Enter to see the time that Socket manager has been created");
+                Console.ReadLine();
+                Console.WriteLine("Date:{0}\n", sm.CreatedAt);
+
+                Console.WriteLine("Press CTRL+C to exit");
+            }
+
+
+        }
+    }
+}
